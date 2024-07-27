dfbe5e85b673e885dec4b5f0ec6534b944626d1a
Suryansh Awasthi <suryansh0010@example.com>
2024-07-27 16:10:54 +0530

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('suryansh awasthi')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>please login at this website</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
